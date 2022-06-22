220e231b197a4652858f70e3045f6b79054d524b
Julio Dutra <jjcdutra@gmail.com>
2022-06-21 21:38:40 -0300

    First step kotlin with spring boot

---
+++ 05_FirstStepsInKotlinwithSpringBoot/kotlin-rest-spring/src/main/kotlin/jjcdutra2015/com/github/Greeting.kt
@@ -0,0 +1,6 @@
+package jjcdutra2015.com.github
+
+class Greeting(
+    val id: Long,
+    val content: String
+)
---
+++ 05_FirstStepsInKotlinwithSpringBoot/kotlin-rest-spring/src/main/kotlin/jjcdutra2015/com/github/GreetingController.kt
@@ -0,0 +1,16 @@
+package jjcdutra2015.com.github
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetingController {
+    val counter: AtomicLong = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
+        return Greeting(counter.incrementAndGet(), "Hello $name")
+    }
+}
